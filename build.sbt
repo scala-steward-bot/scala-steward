edf81a66e0b2244eeae12c53cacfe54db8d735d8
xuwei-k <6b656e6a69@gmail.com>
2026-01-25 20:13:34 +0900

    add

---
+++ build.sbt
@@ -0,0 +1,31 @@
+scalaVersion := "3.8.2-RC1"
+
+evictionErrorLevel := Level.Warn
+
+libraryDependencies += "org.scala-steward" %% "scala-steward-core" % "0.37.1"
+
+scalacOptions ++= Seq(
+  "-deprecation",
+  "-no-indent",
+  "-Wunused:all",
+)
+
+run / fork := true
+
+val projectDir = file(".").getAbsolutePath
+
+TaskKey[Unit]("runSteward") := Def.uncached {
+  (Compile / runMain)
+    .toTask(
+      Seq(
+        Seq(" org.scalasteward.core.Main"),
+        Seq("--workspace", s"$projectDir/workspace"),
+        Seq("--repos-file", s"$projectDir/repos.md"),
+        Seq("--git-author-email", "2517319+scala-steward-bot@users.noreply.github.com"),
+        Seq("--forge-login", "scala-steward-bot"),
+        Seq("--git-ask-pass", s"$projectDir/git_ask_pass.sh"),
+        Seq("--disable-sandbox"),
+      ).flatten.mkString(" ")
+    )
+    .value
+}
---
+++ src/main/scala/scala_steward_bot/GetReposFromGitHubApp.scala
@@ -0,0 +1,50 @@
+package scala_steward_bot
+
+import better.files.File
+import cats.effect.ExitCode
+import cats.effect.IO
+import cats.effect.IOApp
+import cats.effect.Resource
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.StandardOpenOption
+import org.http4s.client.Client
+import org.http4s.syntax.literals.*
+import org.scalasteward.core.client.ClientConfiguration
+import org.scalasteward.core.forge.github.GitHubAuthAlg
+import org.scalasteward.core.util.HttpJsonClient
+import org.typelevel.log4cats.SelfAwareStructuredLogger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
+
+object GetReposFromGitHubApp extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    (for {
+      given SelfAwareStructuredLogger[IO] <- Resource.eval(Slf4jLogger.fromName[IO]("org.scalasteward.core"))
+      given Client[IO] <- ClientConfiguration.build[IO](
+        ClientConfiguration.BuilderMiddleware.default,
+        x => x
+      )
+    } yield {
+      given HttpJsonClient[IO] = new HttpJsonClient[IO]()
+      val alg = new GitHubAuthAlg[IO](
+        uri"https://api.github.com",
+        89853L,
+        File("key.pem")
+      )
+      alg.accessibleRepos
+    }).use { repos =>
+      repos.flatMap { list =>
+        IO {
+          val values = list.map(a => s"- ${a.owner}/${a.repo}").mkString("\n")
+          println(values)
+          Files.writeString(
+            Path.of("repos.md"),
+            values,
+            StandardOpenOption.APPEND
+          )
+          ExitCode.Success
+        }
+      }
+    }
+  }
+}
